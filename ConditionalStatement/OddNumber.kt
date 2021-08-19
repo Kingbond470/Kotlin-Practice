2a280a16cd12979e83290e6bc4f3d1291985a23e
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-08-19 08:53:11 +0530

    Odd Number

---
+++ ConditionalStatement/OddNumber.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+//a number if it is odd print "Odd Number"
+fun main(){
+    val scanner =Scanner(System.`in`)
+    print("Enter the number")
+    val a=scanner.nextInt();
+    val output= if(a%2==1) print("Odd Number") else print("Not odd Number")
+}
