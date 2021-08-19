41b0c62c1749897d90ad75e59b0cd5c64c521b40
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-08-19 09:08:03 +0530

    Reverse Array

---
+++ Array/ReverseArray.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val arr= arrayOf(90, 32, 51, 1, 56)
+    for( i in arr.size-1 downTo 0) println(arr[i])
+
+    val arr1= arrayOf("A", "E", "I", "O", "U")
+    for( i in arr1.size-1 downTo 0) println(arr1[i])
+}
